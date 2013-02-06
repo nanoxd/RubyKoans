f7140b39723fd6f1e487ce0a9b81cb34707adf54
Fernando Paredes <nanoxd@me.com>
2013-02-06 07:27:07 -0800

    Completed the triangle project part 2

--- triangle.rb
+++ triangle.rb
@@ -14,11 +14,20 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b and b == c
+  # WRITE THIS CODE
+  [a,b,c].permutation { |p|
+     if p[0] + p[1] <= p[2]
+       raise TriangleError, "Two sides of a triangle must be greater than the remaining side."
+     elsif p.count { |x| x <= 0} > 0
+       raise TriangleError, "A triangle cannot have sides of zero or less length."
+     end
+  }
+
+  if [a,b,c].uniq.count == 1
     return :equilateral
-  elsif (a == b or a == c or b == c)
+  elsif [a,b,c].uniq.count == 2
     return :isosceles
-  elsif (a != b and a != c)
+  elsif [a,b,c].uniq.count == 3
     return :scalene
   end
 end
